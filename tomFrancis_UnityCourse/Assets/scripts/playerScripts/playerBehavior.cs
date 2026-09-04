6086d6e191c2ae86f7ee1ce369574accc3496541
CaioGui <caioanimationdesign@gmail.com>
2026-09-04 12:23:05 -0300

    click to fire mechanic 1

--- tomFrancis_UnityCourse/Assets/scripts/playerScripts/playerBehavior.cs
+++ tomFrancis_UnityCourse/Assets/scripts/playerScripts/playerBehavior.cs
@@ -3,20 +3,34 @@ using UnityEngine;
 
 public class playerBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ 
+      //Never set the value of a public variable here - the inspector will override it without telling you 
+      public float speed;
+      public GameObject bulletPrefab;
+
     void Start()
     {
-        
+        Debug.Log(speed);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-      float speed = 0.5f;
-      //Debug.Log(Input.GetAxis("Vertical"));
 
-       transform.position += Vector3.forward * Input.GetAxis("Vertical") * speed;
+    //WASD to move
+      float maxDistanceToMove = Time.deltaTime * speed;
 
+       Debug.Log(Input.GetAxis("Vertical"));
+
+       transform.position += Vector3.forward * Input.GetAxis("Vertical") * maxDistanceToMove;
+       transform.position += Vector3.right * Input.GetAxis("Horizontal") * maxDistanceToMove;
+
+    //Click to fire
+    if (Input.GetButton("Fire1"))
+        {
+         Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+    
     }
 }
