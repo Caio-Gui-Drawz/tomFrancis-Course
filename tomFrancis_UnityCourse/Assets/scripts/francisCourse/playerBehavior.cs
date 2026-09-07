d2e6c0ad76b6f3376cfe0f16aaf47da93c3192bb
CaioGui <caioanimationdesign@gmail.com>
2026-09-07 02:29:23 -0300

    firerate mechanic

--- tomFrancis_UnityCourse/Assets/scripts/francisCourse/playerBehavior.cs
+++ tomFrancis_UnityCourse/Assets/scripts/francisCourse/playerBehavior.cs
@@ -8,9 +8,16 @@ public class playerBehavior : MonoBehaviour
       public float speed;
       public GameObject bulletPrefab;
 
+      public float fireRate; //how many seconds between shots 
+
+      float secondsSinceLastShot;
+
     void Start()
     {
-        Debug.Log(speed);
+
+
+        secondsSinceLastShot = fireRate; //inicia podendo atirar
+
     }
 
     // Update is called once per frame
@@ -37,13 +44,19 @@ public class playerBehavior : MonoBehaviour
 
 
           
-    //Click to fire
-    if (Input.GetButton("Fire1"))
+    //Firing
+    secondsSinceLastShot += Time.deltaTime; //vai aumentar esse valor todo segundo ate chegar no fireRate, ai o player vai poder atirar de novo
+
+
+
+    if (secondsSinceLastShot >= fireRate && Input.GetButton("Fire1"))
         {
 
             //cria a bala na posição do player mas com um offset para frente, para não colidir com o player
             //pega o "forward" do transform do player e soma isso com a posicao do player adcionando 1 unidade na frente
          Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
+
+         secondsSinceLastShot = 0; //reseta o contador de tempo para o próximo tiro
         }
     
     }
