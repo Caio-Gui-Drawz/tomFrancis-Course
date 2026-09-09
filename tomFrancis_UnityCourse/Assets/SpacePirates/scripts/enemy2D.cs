005479d4c4601c721065e676acd6852bce405bd4
CaioGui <caioanimationdesign@gmail.com>
2026-09-08 23:35:24 -0300

    enemy deals damage to the player

--- tomFrancis_UnityCourse/Assets/SpacePirates/scripts/enemy2D.cs
+++ tomFrancis_UnityCourse/Assets/SpacePirates/scripts/enemy2D.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class enemy2D : MonoBehaviour
 {
+
+    public float damage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,27 @@ public class enemy2D : MonoBehaviour
     void Update()
     {
         
+    }
+
+     private void OnCollisionEnter2D(Collision2D thisCollision)
+    {
+         Debug.Log("collision detected");
+        GameObject theirGameObject = thisCollision.gameObject; //objeto que bateu no collider da bala.
+        if (theirGameObject.GetComponent<Player2D>() != null)
+        
+        {
+        Debug.Log("player hit");
+        
+        healthSystem theirHealthSystem = theirGameObject.GetComponent<healthSystem>();
+
+        if (theirHealthSystem != null)
+
+        {
+            Debug.Log("player health system found");
+        theirHealthSystem.TakeDamage(damage); 
+        }
+        
+
+        }
     }
 }
