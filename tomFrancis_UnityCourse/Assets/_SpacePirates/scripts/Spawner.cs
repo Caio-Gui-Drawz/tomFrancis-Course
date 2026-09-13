193d00be40c175507e6de9f8f0274468d02f0d6c
CaioGui <caioanimationdesign@gmail.com>
2026-09-13 00:07:51 -0300

    spawn system working for now

--- tomFrancis_UnityCourse/Assets/_SpacePirates/scripts/Spawner.cs
+++ tomFrancis_UnityCourse/Assets/_SpacePirates/scripts/Spawner.cs
@@ -6,11 +6,21 @@ public class Spawner : MonoBehaviour
     public GameObject prefab;
     public Transform[] allowedSpawnPoints;
 
+    public float minAngle, maxAngle;
+
     public void SpawnOne()
     {
         if (allowedSpawnPoints == null || allowedSpawnPoints.Length == 0) return;
+        //Escolhe um angulo aleatorio.
+        float randomAngle = Random.Range(minAngle, maxAngle);
+
+        //Cria uma rotação a partir do angulo aleatorio
+        Quaternion randomRotation = Quaternion.Euler(0, 0, randomAngle);
+
+        //Combina a rotação do spawner com a rotação aleatoria.
+        Quaternion finalRotation = transform.rotation * randomRotation;
 
         Transform spawnPoint = allowedSpawnPoints[Random.Range(0, allowedSpawnPoints.Length)];
-        PoolManager.Instance.Get(prefab, spawnPoint.position, spawnPoint.rotation);
+        PoolManager.Instance.Get(prefab, spawnPoint.position, finalRotation);
     }
 }
